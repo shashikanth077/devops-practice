9565562a95922c30f97f9a712381223831a8c079
shashikanth077 <shashikanth033@gmail.com>
2025-06-06 21:15:59 +0530

    simple test cases

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App.jsx';
+
+test('renders welcome heading', () => {
+  render(<App />);
+  const headingElement = screen.getByText(/Welcome to React App/i);
+  expect(headingElement).toBeInTheDocument();
+});
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learn React/i);
   expect(linkElement).toBeInTheDocument();
 });
